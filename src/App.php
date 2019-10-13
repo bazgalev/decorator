28e46af77ecc5a748d438b01c1917bfe62c0adb6
bazgalev <pazickss@yandex.ru>
2019-10-13 13:01:44 +0300

    init

---
+++ src/App.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mikhail
+ * Date: 13.10.2019
+ * Time: 12:59
+ */
+
+namespace App;
+
+
+class App
+{
+    public function run(): void
+    {
+        echo 'Entry point here!' . PHP_EOL;
+    }
+}
